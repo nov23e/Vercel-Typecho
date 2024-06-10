6dce95fc440f15d09fc2de7bff66b03e93e14df9
chacores <123297759+chacores@users.noreply.github.com>
2024-06-10 10:00:18 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户',
-  'password' => '数据库密码',
+  'host' => 'chacores.42web.io',
+  'user' => ' if0_36703451',
+  'password' => '2020520A',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库名',
+  'database' => 'chacores',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
